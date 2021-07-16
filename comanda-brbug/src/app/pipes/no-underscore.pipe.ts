8aaa7f2b303ad6046d2b36c0ded97ec9a6586e52
frc95 <frc95zz@gmail.com>
2021-07-16 15:32:20 -0300

    Cards arreglados

--- comanda-brbug/src/app/pipes/no-underscore.pipe.ts
+++ comanda-brbug/src/app/pipes/no-underscore.pipe.ts
@@ -8,7 +8,12 @@ export class NoUnderscorePipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): unknown {
 
     if(value!=null){
-      return value.replace("_", " ");
+      if(value=="confirmacion_pago"){
+        return "El mozo está confirmando el pago";
+      }else{
+        let s = value.charAt(0).toUpperCase() + value.slice(1);
+        return s.split('_').join(' ');
+      }
     }
 
   }
